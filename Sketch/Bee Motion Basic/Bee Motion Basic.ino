0b7220d343b39ee55f1327a7afe09b515d0f29de
Paul Price <56952812+strid3r21@users.noreply.github.com>
2022-07-23 12:44:28 -0400

    added a basic sketch
    this basic sketch will let you monitor motion detection from the serial monitor

---
+++ Sketch/Bee Motion Basic/Bee Motion Basic.ino
@@ -0,0 +1,30 @@
+
+#include "WiFi.h"
+#define WLAN_SSID "Your-Wifi"
+#define WLAN_PASS "Your-Wiii-Password"
+
+#define PIR 5  //defining the PIR
+
+void setup() {
+  Serial.begin(115200);
+
+  pinMode(PIR, INPUT);   //Setting the PIR as an Input
+ 
+    WiFi.begin(WLAN_SSID, WLAN_PASS);
+  delay(2000);
+}
+
+
+void loop() {
+ 
+
+  if(digitalRead(PIR) == HIGH){   //if the PIR reads HIGH, then its detecting motion.
+   Serial.println("Motion Detected!!!");   
+  delay(500); 
+  }else{    //if its not reading HIGH (ie, its LOW) then there is no motion detected.
+    Serial.println("No Motion");
+    delay(500); 
+  }
+
+               
+}
